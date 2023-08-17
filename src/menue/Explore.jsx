573a8ec7e4b90803d9746ebda90c485348ba640b
Aliyu Abdulrazak <106888664+Razak002@users.noreply.github.com>
2023-08-17 15:58:40 +0100

    Update Explore.jsx
    updated the github linlk

--- src/menue/Explore.jsx
+++ src/menue/Explore.jsx
@@ -6,7 +6,7 @@ function Explore() {
     <div className='w-screen h-60 flex md:text-xl justify-center items-center md:p-52 '>
         <h2 className=' text-white'>Explore more on my Github page </h2>
         <div className='ml-9 mt-3 down-arrow'>
-            <a href='https://github.com/Razak002/commerce' >
+            <a href='https://github.com/Razak002' >
                 <FaArrowRight className='text-gray-400 text-2xl animate-bounce' />
                 </a>
             </div>
