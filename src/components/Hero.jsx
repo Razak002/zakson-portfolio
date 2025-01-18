b14982a2d7496a92fe58e769c03f93c79e63e747
Aliyu Abdulrazak <106888664+Razak002@users.noreply.github.com>
2025-01-18 13:34:36 +0100

    fixes: some little necessarry touches

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,18 +6,15 @@ function Hero() {
   return (
     <div id="main" className="bg-gradient-to-r min-h-screen flex items-center justify-center">
       <div className="p-6 md:flex md:items-center md:justify-between md:gap-10  max-w-6xl mx-auto">
-        
-        {/* Text Section */}
+
         <div className="md:w-1/2 text-center md:text-left space-y-5">
           <h1 className="text-white text-4xl md:text-5xl lg:text-6xl font-serif font-bold">
             Aliyu Abdulrazak
           </h1>
           <p className="text-white text-lg md:text-xl font-kalam animate__animated animate__fadeInLeft animate__slow">
-            I'm a productive Frontend Developer with great passions. I am self-motivated, enthusiastic, and always ready to work and learn.
+            I'm a productive Software Developer with great passions. I am self-motivated, enthusiastic, and always ready to work in teams and to learn more.
           </p>
         </div>
-
-        {/* Image Section */}
         <div className="mt-8 md:mt-0 md:w-1/2 flex justify-center overflow-hidden">
           <a href="https://ibb.co/vv5r3h1" target="_blank" rel="noopener noreferrer">
             <img
